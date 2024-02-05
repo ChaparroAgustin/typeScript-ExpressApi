d0919a05966f2a8d0b20c48f8fab40fe2b997cd0
ChaparroAgustin <chaparroagustin91@hotmail.com>
2024-02-05 13:30:02 -0300

    inicio

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json());// middleware que transforma la req.body a un json
 
 const PORT = 3000; 
 
-app.get('./ping', (req, res) => {
+app.get('./ping', (_req, res) => {
     console.log('someone pinged here!!!')
     res.send('pong');
 })
